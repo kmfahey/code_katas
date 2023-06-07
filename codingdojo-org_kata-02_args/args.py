b4cebb056c3030ceed7702b6021d85c014555d2a
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-06-07 16:17:59 -0700

    implemented further tests; commented args.py

--- codingdojo-org_kata-02_args/args.py
+++ codingdojo-org_kata-02_args/args.py
@@ -6,6 +6,11 @@ import re
 __all__ = ["CLArg_Parser"]
 
 
+
+# Implements a parser that can be configured with a set of flags and their data
+# types. Once the schema is set up, calling parse() with an argv populates the
+# state with flag settings. Thereafter the state can be queries using accessor
+# methods.
 class CLArg_Parser:
     __slots__ = "flags_schema", "flag_args", "nonflag_args", "nonflag_allowed"
 
@@ -18,31 +23,39 @@ class CLArg_Parser:
         self.nonflag_args = list()
         self.nonflag_allowed = nonflag_allowed
 
+    # Add a flag to the schema.
     def add_flag(self, flag, argtype=bool):
         self.flags_schema[flag] = argtype
 
+    # Get a list of all flags in the schema.
     def get_flags(self):
         return list(self.flags_schema.keys())
 
+    # Get a list of all flags that have values set.
     def get_flags_set(self):
         return list(self.flag_args.keys())
 
+    # Get the value of a flag.
     def get_flag_val(self, flag):
         if flag not in self.flag_args:
             return None
         else:
             return self.flag_args[flag]
 
+    # Get the arguments that were left in argv after all the flags and their
+    # args were accounted for.
     def get_nonflag_args(self):
         if not self.nonflag_allowed:
             return None
         else:
             return self.nonflag_args.copy()
 
+    # Empty state values set by parsing so parse() can be called again.
     def reset(self):
         self.flag_args.clear()
         self.nonflag_args.clear()
 
+    # Internal method that's used to cast a flag argument to its expected type.
     def _cast_flag_arg(self, flag, argval):
         if self.flags_schema[flag] is bool:
             if argval is None or argval is True:
@@ -57,29 +70,40 @@ class CLArg_Parser:
                 raise ValueError(f"flag -{flag} requires an argument of type '{argtype.__name__}'")
             return value
 
+    # Parses an argv and sets internal state with the flags found
     def parse(self, argv):
-        if self.flag_args or self.nonflag_args:
+        if not self.flag_schema:
+            raise ValueError("no flags set in schema: can't parse argv")
+        elif self.flag_args or self.nonflag_args:
             raise ValueError("object flags state already set; cannot parse another argv while state is set")
         arg_vector = argv.copy()
         while len(arg_vector):
             next_arg = arg_vector.pop(0)
-            if re.match("^-([A-Za-z][^A-Za-z]*)+$", next_arg):
-                self.add_flag_cluster(next_arg)
+            if re.match("^-([A-Za-z][^A-Za-z]*){2,}$", next_arg):
+                self._add_flag_cluster(next_arg)
             elif re.match("^-[A-Za-z]", next_arg):
-                self.add_single_flag(next_arg, arg_vector)
+                self._add_single_flag(next_arg, arg_vector)
             else:
-                self.add_nonflag_args(self, arg_vector)
+                if any(re.match("^-[A-Za-z]", arg) for arg in arg_vector):
+                    flag_arg = next(arg for arg in arg_vector if re.match("^-[A-Za-z]", arg))
+                    raise ValueError(f"nonflag argument {next_arg} encountered in argv followed by flag argument {flag_arg}")
+                self._add_nonflag_arg(next_arg)
 
-    def add_nonflag_args(self, arg_vector):
-        if not self.nonflag_allowed:
+    # Adds an argument to the nonflag args list.
+    def _add_nonflag_arg(self, next_arg):
+        if self.nonflag_allowed:
+            self.nonflag_args.append(next_arg)
+        else:
             nonflag_args_expr = ", ".join(f"'{arg}'" for arg in arg_vector)
             if len(nonflag_args_expr) > 70:
                 nonflag_args_expr = nonflag_args_expr[:67] + "..."
-            raise ValueError(f"commandline arguments include nonflag args ({nonflag_args_expr}) "
-                             "which are not allowed by the schema")
-        self.nonflag_args[:] = arg_vector[:]
+            raise ValueError(f"commandline arguments included nonflag arguments ({nonflag_args_expr}) "
+                             "which is not allowed by the schema")
 
-    def add_flag_cluster(self, arg_cluster):
+    # Unpack a cluster of flags (i.e. a string beginning with '-' followed by
+    # more than one alphabetic flags, optionally with arguments, packed into a
+    # string with no spaces) and set them in state individually.
+    def _add_flag_cluster(self, arg_cluster):
         arg_cluster = arg_cluster.removeprefix("-")
         flags_w_args = re.split("(?=[A-Za-z][^A-Za-z]*)", arg_cluster)[1:]
         arg_cluster_d = {match_obj.group(1): match_obj.group(2)
@@ -92,8 +116,9 @@ class CLArg_Parser:
             arg_cluster_d[flag] = self._cast_flag_arg(flag, argval)
         self.flag_args.update(arg_cluster_d)
 
-    def add_single_flag(self, arg_flag, arg_vector):
-        flag = arg_flag.rstrip("-")
+    # Add a single flag to internal state.
+    def _add_single_flag(self, arg_flag, arg_vector):
+        flag = arg_flag.removeprefix("-")
         if self.flags_schema[flag] is bool:
             if len(arg_vector) and not arg_vector[0].startswith("-"):
                 raise ValueError(f"flag -{flag} doesn't take an argument (was passed the argument '{arg_vector[0]}'")
--- codingdojo-org_kata-02_args/test_args.py
+++ codingdojo-org_kata-02_args/test_args.py
@@ -13,7 +13,37 @@ def test_par_like_cl_flags():
     assert cl_parser.get_flag_val("q") is True
     assert cl_parser.get_flag_val("s") == 0
     assert cl_parser.get_flag_val("w") == 78
+    assert cl_parser.get_nonflag_args() is None
+
+def test_other_flags():
+    cl_parser = CLArg_Parser(nonflag_allowed=False)
+    cl_parser.add_flag('B', int)
+    cl_parser.add_flag('N', str)
+    cl_parser.add_flag('x', float)
+    cl_parser.add_flag('c', bool)
+    cl_parser.add_flag('r', bool)
+    argv = [arg.strip("'") for arg in "-B 1 -N 'foo' -x 0.5 -cr".split()]
+    cl_parser.parse(argv)
+    assert cl_parser.get_flag_val('B') == 1
+    assert cl_parser.get_flag_val('N') ==  "foo"
+    assert cl_parser.get_flag_val('x') == 0.5
+    assert cl_parser.get_flag_val('c') == True
+    assert cl_parser.get_flag_val('r') == True
+    assert cl_parser.get_nonflag_args() is None
+
+def test_flags_w_nonflag_args():
+    cl_parser = CLArg_Parser(nonflag_allowed=True)
+    cl_parser.add_flag('O', bool)
+    cl_parser.add_flag('N', str)
+    cl_parser.add_flag('F', bool)
+    cl_parser.add_flag('I', float)
+    cl_parser.add_flag('D', int)
+    argv = [arg.strip("'") for arg in "-O -N 'foo' -F -I 0.5 -D 1 'foo' 'bar' 'baz'".split()]
+    cl_parser.parse(argv)
+    assert cl_parser.get_nonflag_args() == ["foo", "bar", "baz"]
 
 
 if __name__ == "__main__":
     test_par_like_cl_flags()
+    test_other_flags()
+    test_flags_w_nonflag_args()
