efe20a109a9019b7227b0a7e7029de6ab15ae9b8
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-06-11 16:38:47 -0700

    implemented main() function with basic functionality

--- codingdojo-org_kata-04_birthday-greetings/birthday_greetings.py
+++ codingdojo-org_kata-04_birthday-greetings/birthday_greetings.py
@@ -33,15 +33,47 @@ import csv
 import datetime
 import pytz
 import abc
+import sys
+import os.path
 
 
-class Friends_Data_Loader(metaclass=abc.ABCMeta):
+def main():
+    if len(sys.argv) == 1:
+        print("Please specify a csv file on the commandline.")
+        exit(1)
+    elif len(sys.argv) > 2:
+        print("Please specify only one file on the commandline.")
+        exit(1)
+    filename = sys.argv[1]
+    if not filename.lower().endswith(".csv"):
+        print("Please specify a csv file on the commandline.")
+        exit(1)
+    elif not os.path.exists(filename):
+        print(f"File {filename} does not exist.")
+        exit(1)
+
+    csv_loader = Csv_Loader(filename)
+
+    friends_list = Friends_List()
+    friends_list.add_from_loader(csv_loader)
+
+    bday_today_friends = friends_list.friends_w_birthday_today()
+
+    sender_obj = Mail_Sender("Fahey", "Kerne", "kernefahey@protonmail.com")
+
+    bday_msgs = [sender_obj.gen_bday_msg(friend_obj) for friend_obj in bday_today_friends]
+
+    for bday_msg in bday_msgs:
+        sender_obj.send_message(bday_msg)
+
+
+class Data_Loader(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __init__(self):
         pass
 
 
-class Csv_Reader(Friends_Data_Loader):
+class Csv_Loader(Data_Loader):
     __slots__ = "reader", "columns"
 
     def __init__(self, filename):
@@ -54,6 +86,7 @@ class Csv_Reader(Friends_Data_Loader):
     def __iter__(self):
         for line in self.reader:
             line_d = dict(zip(self.columns, line))
+            line_d["date_of_birth"] = datetime.date(*map(int, line_d["date_of_birth"].split('/')))
             yield line_d
 
 
@@ -62,6 +95,10 @@ class Friends_List:
 
     def __init__(self):
         self._friend_objs = list()
+
+    def add_from_loader(self, loader_obj):
+        for friend_d in loader_obj:
+            self.add_friend(friend_d)
 
     def add_friend(self, friend_d):
         self._friend_objs.append(Friend(**friend_d))
@@ -83,14 +120,15 @@ class Friend:
     __slots__ = "last_name", "first_name", "date_of_birth", "email"
 
     def __init__(self, last_name, first_name, date_of_birth, email):
+        if not isinstance(date_of_birth, datetime.date):
+            raise ValueError("date_of_birth argument to Friend constructor must be a datetime.date object")
         self.last_name = last_name
         self.first_name = first_name
         self.date_of_birth = date_of_birth
         self.email = email
 
     def __repr__(self):
-        return (f"Friend({repr(self.last_name)}, {repr(self.first_name)}, "
-                        "{repr(self.date_of_birth)}, {repr(self.email)})")
+        return (f"Friend({repr(self.last_name)}, {repr(self.first_name)}, {repr(self.date_of_birth)}, {repr(self.email)})")
 
 
 class Message(metaclass=abc.ABCMeta):
@@ -109,6 +147,9 @@ class Mail_Message(Message):
         self.subject_line = subject_line
         self.message_body = message_body
 
+    def __repr__(self):
+        return f"Mail_Message({repr(self.from_line)}, {repr(self.date_line)}, {repr(self.to_line)}, {repr(self.subject_line)}, {repr(self.message_body)})"
+
 
 class Message_Sender(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -121,14 +162,18 @@ class Message_Sender(metaclass=abc.ABCMeta):
 
 
 class Mail_Sender(Message_Sender):
-    def __init__(self, from_name, from_address):
-        pass
+    __slots__ = "from_last_name", "from_first_name", "from_address"
+
+    def __init__(self, from_last_name, from_first_name, from_address):
+        self.from_last_name = from_last_name
+        self.from_first_name = from_first_name
+        self.from_address = from_address
 
     def gen_bday_msg(self, friend_obj):
         timezone = pytz.timezone('America/Los_Angeles')
         todays_dt = datetime.datetime.now(timezone)
 
-        from_line = f"From: {self.from_name} <{self.from_address}"
+        from_line = f"From: {self.from_first_name} {self.from_last_name} <{self.from_address}"
         date_line = todays_dt.strftime("Date: %-m/%-d/%y %-I:%M (GMT%z)")
         to_line = f"To: {friend_obj.first_name} {friend_obj.last_name} <{friend_obj.email}>"
         subject_line = "Subject: Happy Birthday!"
@@ -137,4 +182,8 @@ class Mail_Sender(Message_Sender):
         return Mail_Message(from_line, date_line, to_line, subject_line, message_body)
 
     def send_message(self, message_obj):
-        return True
+        print(f"Sending message {repr(message_obj)}.")
+
+
+if __name__ == "__main__":
+    main()
