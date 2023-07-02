b1366bac157815b3770c39236aabb811bbdd7eec
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-01 22:04:58 -0700

    utility script expanded to create & manage sqlite db as q&d trial

--- codingdojo-org_kata-04_birthday-greetings/_birthdays_csv_test_data_generator.py
+++ codingdojo-org_kata-04_birthday-greetings/_birthdays_csv_test_data_generator.py
@@ -1,13 +1,58 @@
 #!/usr/bin/python3
 
-import faker
-import datetime
-import operator
+import sqlite3
+import os.path
 
 from test_birthday_greetings import generate_testing_data
 
 
 table = generate_testing_data()
 
-for row in table:
-    print(*row, sep=", ")
+with open("birthdays2.csv", "w") as bday_fh:
+    for row in table:
+        print(*row, sep=", ", file=bday_fh)
+
+print("birthdays2.csv created")
+
+print(f"{len(table)-1} rows saved")
+
+db_name = "birthdays2_sqlite.db"
+
+if not os.path.exists(db_name):
+    db_conx = sqlite3.connect(db_name)
+    print(f"{db_name} created")
+else:
+    db_conx = sqlite3.connect(db_name)
+    print(f"{db_name} connected to")
+
+db_curs = db_conx.cursor()
+
+result = db_curs.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='table_name';")
+
+if not result:
+    db_curs.execute("""CREATE TABLE IF NOT EXISTS birthdays (
+                            last_name VARCHAR(64),
+                            first_name VARCHAR(64),
+                            date_of_birth VARCHAR(10),
+                            email VARCHAR(64)
+                       );""")
+    print("table birthdays created")
+else:
+    result = db_curs.execute("DELETE FROM birthdays;")
+    rows_deleted = result.rowcount
+    print("birthdays table cleared")
+    print(f"{rows_deleted} rows deleted")
+    db_conx.commit()
+
+table_iter = iter(table)
+
+# skip the first row, which is the header row of the csv
+next(table_iter)
+
+for last_name, first_name, date_of_birth, email in table_iter:
+    year, month, day = date_of_birth.split('/')
+    db_curs.execute(f"""INSERT INTO birthdays (last_name, first_name, date_of_birth, email)
+                        VALUES ('{last_name}', '{first_name}', '{year}-{month}-{day}', '{email}');""")
+    db_conx.commit()
+
+print(f"{len(table)-1} rows inserted into table `birthdays`")
