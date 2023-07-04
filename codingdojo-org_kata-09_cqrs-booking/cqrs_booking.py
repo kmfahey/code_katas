8d06d9545d7258f7d4362f6b2fc75a08f3db1837
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-04 16:53:19 -0700

    re-implemented bookings tracking to support multiple bookings per room

--- codingdojo-org_kata-09_cqrs-booking/cqrs_booking.py
+++ codingdojo-org_kata-09_cqrs-booking/cqrs_booking.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from datetime import date
+from datetime import date, timedelta
 
 from dataclasses import dataclass
 
@@ -18,35 +18,63 @@ class Room:
     room_number: int
 
 
-@dataclass
+# Making the Booking class immutable so sets can be used with it.
+@dataclass(frozen=True)
 class Booking:
     client_id: int
     room_number: int
     arrival_date: date
     departure_date: date
 
+    def gen_dates_booked(self):
+        duration = (self.departure_date - self.arrival_date).days
+        retval = [self.arrival_date + timedelta(days=day_number) for day_number in range(0, duration)]
+        return retval
+
 
 class Bookings_Register:
-    __slots__ = "rooms", "bookings"
+    __slots__ = "room_bookings", 
 
     def __init__(self, rooms_per_floor=()):
         self.room_bookings = dict()
         for floor_number, rooms_on_this_floor in enumerate(rooms_per_floor, start=1):
             first_room_number = floor_number * 100
-            last_room_number = 100 + rooms_on_this_floor - 1
+            last_room_number = first_room_number + rooms_on_this_floor - 1
             for room_number in range(first_room_number, last_room_number + 1):
                 room_obj = Room(room_number)
-                self.room_bookings[room_obj] = None
+                self.room_bookings[room_obj] = dict()
 
     def add_booking(self, room_number, client_id, arrival_date, departure_date):
+        room_obj = Room(room_number)
         booking_obj = Booking(client_id, room_number, arrival_date, departure_date)
-        self.bookings[Room(room_number)] = booking_obj
+        dates_booked = booking_obj.gen_dates_booked
+        for day_booked in dates_booked:
+            self.room_bookings[room_obj][day_booked] = booking_obj
 
-    def get_bookings(self, room_number):
-        return self.bookings[Room(room_number)]
+    def delete_booking(self, room_number, day_booked):
+        room_obj = Room(room_number)
+        booking_obj_to_del = None
+        for booking_obj in set(self.bookings[room_obj].values()):
+            if booking_obj.arrival_date <= day_booked <= booking_obj.departure_date:
+                booking_obj_to_del = booking_obj
+                break
+        if booking_obj_to_del is None:
+            return 0
+        days_occupied = [day_occupied for day_occupied, booking_obj in self.bookings[room_obj].items()
+                         if booking_obj is booking_obj_to_del]
+        for day_occupied in days_occupied:
+            del booking_obj_to_del[day_occupied]
+        return len(days_occupied)
 
-    def delete_booking(self, room_number):
-        self.bookings[Room(room_number)] = None
+    def get_booking(self, room_number, day_of_interest):
+        room_obj = Room(room_number)
+        if day_of_interest in self.bookings[room_obj]:
+            return self.bookings[room_obj][day_of_interest]
+        return None
 
-    def get_booking(self, room_number):
-        return self.bookings[Room(room_number)]
+    def get_booked_room_numbers(self, day_of_interest=date.today()):
+        return [room_obj.room_number for room_obj in self.bookings if day_of_interest in self.bookings[room_obj]]
+
+    def get_free_room_numbers(self, day_of_interest=date.today()):
+        return [room_obj.room_number for room_obj in self.bookings if day_of_interest not in self.bookings[room_obj]]
+
