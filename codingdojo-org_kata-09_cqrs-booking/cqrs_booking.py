41fc70f4ce25bb3d37f6d652c1299b2d9a4e3673
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-06 13:14:51 -0700

    updated tests to test date-aware bookings, fixed bugs in same

--- codingdojo-org_kata-09_cqrs-booking/cqrs_booking.py
+++ codingdojo-org_kata-09_cqrs-booking/cqrs_booking.py
@@ -26,55 +26,80 @@ class Booking:
     arrival_date: date
     departure_date: date
 
+    # returns a list of every date between arrival_date and departure_date, inclusive
     def gen_dates_booked(self):
         duration = (self.departure_date - self.arrival_date).days
         retval = [self.arrival_date + timedelta(days=day_number) for day_number in range(0, duration)]
         return retval
 
 
+# A class that encapsulates a data structure that associates Room objects with
+# Booking objects in a calendar-aware fashion, presents methods for interacting
+# with that data structure.
 class Bookings_Register:
-    __slots__ = "room_bookings", 
+    __slots__ = "bookings",
 
     def __init__(self, rooms_per_floor=()):
-        self.room_bookings = dict()
+        self.bookings = dict()
         for floor_number, rooms_on_this_floor in enumerate(rooms_per_floor, start=1):
             first_room_number = floor_number * 100
             last_room_number = first_room_number + rooms_on_this_floor - 1
             for room_number in range(first_room_number, last_room_number + 1):
                 room_obj = Room(room_number)
-                self.room_bookings[room_obj] = dict()
+                self.bookings[room_obj] = dict()
 
-    def add_booking(self, room_number, client_id, arrival_date, departure_date):
+    # interprets arguments into a room object and a booking object. associates
+    # that booking with that room for all dates between arrival_date and
+    # departure_date.
+    def add_booking(self, client_id, room_number, arrival_date, departure_date):
         room_obj = Room(room_number)
         booking_obj = Booking(client_id, room_number, arrival_date, departure_date)
-        dates_booked = booking_obj.gen_dates_booked
+        dates_booked = booking_obj.gen_dates_booked()
         for day_booked in dates_booked:
-            self.room_bookings[room_obj][day_booked] = booking_obj
+            self.bookings[room_obj][day_booked] = booking_obj
 
+    # Accepts a room number and a day on which the room is booked. day_booked
+    # can be any date between arrival_date and departure_date on the original
+    # booking; it will be used to find the booking by seeking one where
+    # day_booked falls between arrival and departure inclusive. Returns the
+    # number of days freed up by the deletion. If no such booking was found
+    # returns 0.
     def delete_booking(self, room_number, day_booked):
         room_obj = Room(room_number)
         booking_obj_to_del = None
+        # Seeking a booking where day_booked falls between arrival_date and
+        # depature_date inclusive
         for booking_obj in set(self.bookings[room_obj].values()):
             if booking_obj.arrival_date <= day_booked <= booking_obj.departure_date:
                 booking_obj_to_del = booking_obj
                 break
         if booking_obj_to_del is None:
+            # No booking was found.
             return 0
         days_occupied = [day_occupied for day_occupied, booking_obj in self.bookings[room_obj].items()
                          if booking_obj is booking_obj_to_del]
+        # Deletes every day between arrival_date and departure_date inclusive
+        # from the room object's dict in the bookings data structure.
         for day_occupied in days_occupied:
-            del booking_obj_to_del[day_occupied]
+            del self.bookings[room_obj][day_occupied]
         return len(days_occupied)
 
+    # Finds the booking in the room object's place in the bookings data
+    # structure where the day_of_interest falls between arrival_date and
+    # departure_date inclusive.
     def get_booking(self, room_number, day_of_interest):
         room_obj = Room(room_number)
         if day_of_interest in self.bookings[room_obj]:
             return self.bookings[room_obj][day_of_interest]
         return None
 
+    # Returns the room number of every room that is booked on the day of
+    # interest.
     def get_booked_room_numbers(self, day_of_interest=date.today()):
         return [room_obj.room_number for room_obj in self.bookings if day_of_interest in self.bookings[room_obj]]
 
+    # Returns the room number of every room that is open on the day of
+    # interest.
     def get_free_room_numbers(self, day_of_interest=date.today()):
         return [room_obj.room_number for room_obj in self.bookings if day_of_interest not in self.bookings[room_obj]]
 
--- codingdojo-org_kata-09_cqrs-booking/test_cqrs_booking.py
+++ codingdojo-org_kata-09_cqrs-booking/test_cqrs_booking.py
@@ -18,3 +18,31 @@ def test_booking_class():
     assert booking_obj.arrival_date == today
     assert booking_obj.departure_date == tomorrow
 
+def test_bookings_register_init():
+    def get_max_room_on_floor(room_bookings, floor_number):
+        room_number = floor_number * 100
+        while Room(room_number + 1) in room_bookings:
+            room_number += 1
+        return room_number
+    bookings_reg_obj = Bookings_Register((40, 40, 40, 40, 40))
+    for floor_number in (1, 2, 3, 4, 5):
+        assert get_max_room_on_floor(bookings_reg_obj.bookings, floor_number) % 100 == 39
+
+def test_bookings_register_add_get_delete_booking_booked_rooms_free_rooms():
+    today = date.today()
+    tomorrow = today + timedelta(days=1)
+    bookings_reg_obj = Bookings_Register((40, 40, 40, 40, 40))
+    client_id = 216
+    room_number = 117
+    bookings_reg_obj.add_booking(client_id, room_number, today, tomorrow)
+    assert room_number in bookings_reg_obj.get_booked_room_numbers()
+    booking_obj = bookings_reg_obj.get_booking(room_number, today)
+    assert booking_obj.client_id == client_id
+    assert booking_obj.room_number == room_number
+    assert booking_obj.arrival_date == today
+    assert booking_obj.departure_date == tomorrow
+    bookings_reg_obj.delete_booking(room_number, today)
+    assert room_number in bookings_reg_obj.get_free_room_numbers()
+
+
+
