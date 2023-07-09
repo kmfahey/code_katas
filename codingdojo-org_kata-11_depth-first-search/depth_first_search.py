483fe7b77efbd76b9cd22e8f967aa2a86ebc2c37
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-09 13:21:38 -0700

    some global name changes

--- codingdojo-org_kata-11_depth-first-search/depth_first_search.py
+++ codingdojo-org_kata-11_depth-first-search/depth_first_search.py
@@ -5,9 +5,9 @@ from collections import OrderedDict
 from math import inf
 
 
-__all__ = "dfs", "adjmx_adjacent_nodes", "adjl_adjacent_nodes", "edgl_adjacent_nodes", "incmx_adjacent_nodes", "adjmap_adjacent_nodes"
+__all__ = "dfs", "adjmx_adj_nodes", "adjl_adj_nodes", "edgl_adj_nodes", "incmx_adj_nodes", "adjmap_adj_nodes"
 
-def adjmx_adjacent_nodes(graph, this_node):
+def adjmx_adj_nodes(graph, this_node):
     adjrow = graph[this_node - 1]
     other_nodes = list()
     for index in range(len(adjrow)):
@@ -15,11 +15,11 @@ def adjmx_adjacent_nodes(graph, this_node):
             other_nodes.append(index + 1)
     return other_nodes
 
-def adjl_adjacent_nodes(graph, this_node):
+def adjl_adj_nodes(graph, this_node):
     index = this_node - 1
     return sorted(graph[index])
 
-def edgl_adjacent_nodes(graph, this_node):
+def edgl_adj_nodes(graph, this_node):
     other_nodes = list()
     for edge in graph:
         left_node, right_node = edge
@@ -29,23 +29,24 @@ def edgl_adjacent_nodes(graph, this_node):
             other_nodes.append(left_node)
     return sorted(other_nodes)
 
-def incmx_adjacent_nodes(graph, node):
+def incmx_adj_nodes(graph, node):
     node_index = node - 1  # adjust for 0-indexing
-    adjacent_nodes = []
+    adj_nodes = []
     for edge_index in range(len(graph[node_index])):
         if graph[node_index][edge_index] == 1:  # If node is part of edge
             # find the adjacent node(s)
             for other_node_index in range(len(graph)):
                 if other_node_index != node_index and graph[other_node_index][edge_index] == 1:
                     # Append node number, adjusting for 1-indexing
-                    adjacent_nodes.append(other_node_index + 1)
-    return adjacent_nodes
+                    adj_nodes.append(other_node_index + 1)
+    return adj_nodes
 
-def adjmap_adjacent_nodes(graph, this_node):
+def adjmap_adj_nodes(graph, this_node):
     index = this_node - 1
     return sorted(graph[index].keys())
 
-def dfs(graph, this_node, adjacent_edges):
+# iterative
+def dfs(graph, this_node, adj_nodes_func):
     stack = list()
     seen = OrderedDict()
     stack.append(this_node)
@@ -53,6 +54,6 @@ def dfs(graph, this_node, adjacent_edges):
         this_node = stack.pop()
         if this_node not in seen:
             seen[this_node] = None
-            for other_node in adjacent_edges(graph, this_node):
+            for other_node in adj_nodes_func(graph, this_node):
                 stack.append(other_node)
     return list(seen.keys())
