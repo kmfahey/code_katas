8daeaf0e4bfbf083ba1845a3a2b54210ae8f4413
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-11 14:53:28 -0700

    expanded dict_replace() with input validation

--- codingdojo-org_kata-13_dictionary-replacer/dictionary_replacer.py
+++ codingdojo-org_kata-13_dictionary-replacer/dictionary_replacer.py
@@ -4,7 +4,19 @@ import re
 
 
 def dict_replace(string, dictionary):
+    if any(s := re.fullmatch(r"(.*\W.*)", key) for key in dictionary):
+        errant_token = s.group(1)
+        raise ValueError("$-bounded tokens must consist of alphabetic characters, numeric characters, "
+                         f"and the underscore character (dictionary contains '{errant_token}')")
     tokens = set(re.findall(r"\$(\w+)\$", string))
+    test_string = re.subn(r"\$(\w+)\$", "", string)[0]
+    if "$" in test_string:
+        if test_string.count('$') == 1:
+            raise ValueError("odd numbered count of symbol '$' in string argument")
+        else:
+            faulty_token = test_string.split('$')[1]
+            raise ValueError("$-bounded tokens must consist of alphabetic characters, numeric characters, "
+                             f"and the underscore character (string contains '{faulty_token}')")
     for token in tokens:
         if token not in dictionary:
             raise RuntimeError(f"token {token} found in string not present in dictionary")
