9b9f69d1801c63aee42789570cbc5dbfdf9c4057
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-13 15:29:06 -0700

    replaced SGAK's free queens func with ChatGPT's from the BFS solution

--- codingdojo-org_kata-14_eight-queens/eight_queens_sim_anneal.py
+++ codingdojo-org_kata-14_eight-queens/eight_queens_sim_anneal.py
@@ -6,6 +6,9 @@ import numpy as numpy
 import random
 
 
+from eight_queens_util import free_queens_kmf as free_queens
+
+
 # This code copied wholesale from "Simulated Annealing and the Eight Queen Problem"
 # <https://towardsdatascience.com/simulated-annealing-and-the-eight-queen-problem-10f737edbb7e>,
 # a Medium article by Sebastián Gerard Aguilar Kleimann <https://medium.com/@sgerardak>.
@@ -15,64 +18,74 @@ import random
 # contact the original author to work out permission and licensing issues.
 
 
-__all__ = "eight_queens_sim_anneal",
+__all__ = "eight_queens_sim_anneal", "free_queens_sgak"
 
 
-# Defining the objective function
-def free_queens(position):
+# This objective function has been replaced with
+# eight_queens_util.free_queens_kmf(), which in a parallel test of 10000 random
+# position arrays, consistently returned a number of free queens that was less
+# than or equal to free_queens_sgak()'s measurement, and never returned a value
+# that was greater than free_queens_sgak()'s measurement. Between the two I
+# think it's this function that has the bug.
 
-    # We start the count
-    no_attack_on_j = 0
-    queen_not_attacking = 0
-
-    # Compare for each pair of queens
-    for i in range(len(position) - 1):
-
-        no_attack_on_j = 0
-
-        for j in range(i + 1, len(position)):
-
-            # Check if there is any diagonal or horizontal attack. Iterative
-            # process for each column.
-            if position[j] != position[i] \
-                    and position[j] != position[i] + (j - i) \
-                    and position[j] != position[i] - (j - i):
-
-                # If there isn't any attack on the evaluated column. The count
-                # is increased by one. This counter is only used as a reference.
-                no_attack_on_j += 1
-
-                # If there is no attack on all the columns. The general counter
-                # is increased by one. This counter indicates the number of
-                # queens that are correctly positioned.
-                if no_attack_on_j == len(position) - 1 - i:
-                    queen_not_attacking += 1
-
-    # The return number is the number of queens not attacking each
-    # other. If this number is 7 we add 1 cause it means the last
-    # queen is also free of attack.
-    if queen_not_attacking == 7:
-        queen_not_attacking += 1
-
-    return queen_not_attacking
+# # Defining the objective function
+# def free_queens_sgak(position):
+# 
+#     # We start the count
+#     no_attack_on_j = 0
+#     queen_not_attacking = 0
+# 
+#     # Compare for each pair of queens
+#     for i in range(len(position) - 1):
+# 
+#         no_attack_on_j = 0
+# 
+#         for j in range(i + 1, len(position)):
+# 
+#             # Check if there is any diagonal or horizontal attack. Iterative
+#             # process for each column.
+#             if position[j] != position[i] \
+#                     and position[j] != position[i] + (j - i) \
+#                     and position[j] != position[i] - (j - i):
+# 
+#                 # If there isn't any attack on the evaluated column. The count
+#                 # is increased by one. This counter is only used as a reference.
+#                 no_attack_on_j += 1
+# 
+#                 # If there is no attack on all the columns. The general counter
+#                 # is increased by one. This counter indicates the number of
+#                 # queens that are correctly positioned.
+#                 if no_attack_on_j == len(position) - 1 - i:
+#                     queen_not_attacking += 1
+# 
+#     # The return number is the number of queens not attacking each
+#     # other. If this number is 7 we add 1 cause it means the last
+#     # queen is also free of attack.
+#     if queen_not_attacking == 7:
+#         queen_not_attacking += 1
+# 
+#     return queen_not_attacking
 
 
 def eight_queens_sim_anneal():
-    # Assign the objective function to "CustomFitness" method.
-    objective= mlrose.CustomFitness(free_queens)
+    best_objective = 0.0
 
-    #Description of the problem
-    problem = mlrose.DiscreteOpt(length=8, fitness_fn=objective, maximize=True, max_val=8)
+    while best_objective != 8.0:
+        # Assign the objective function to "CustomFitness" method.
+        objective= mlrose.CustomFitness(free_queens)
 
-    # Define decay schedule
-    T = mlrose.ExpDecay()
+        #Description of the problem
+        problem = mlrose.DiscreteOpt(length=8, fitness_fn=objective, maximize=True, max_val=8)
 
-    # Define initial state
-    initial_position = numpy.array(range(8))
-    random.shuffle(initial_position)
+        # Define decay schedule
+        T = mlrose.ExpDecay()
 
-    # Solve problem using simulated annealing
-    best_position, best_objective=mlrose.simulated_annealing(problem=problem, schedule=T, max_attempts=500, max_iters=5000,
-                                                             init_state=initial_position)
+        # Define initial state
+        initial_position = numpy.array(range(8))
+        random.shuffle(initial_position)
+
+        # Solve problem using simulated annealing
+        best_position, best_objective = mlrose.simulated_annealing(problem=problem, schedule=T, max_attempts=500, max_iters=5000,
+                                                                   init_state=initial_position)
 
     return(best_position)
--- codingdojo-org_kata-14_eight-queens/eight_queens_util.py
+++ codingdojo-org_kata-14_eight-queens/eight_queens_util.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 
-__all__ = "is_threatened",
+__all__ = "is_threatened", "free_queens_kmf"
 
 
 # Given a new position for the latest queen to be placed at, returns True if
 # the new queen would be threatened there, False if she would not be.
-def is_threatened(positions, new_queen_pos):
+def is_threatened(left_positions, new_queen_pos, right_positions=()):
 
-    for i, queen_pos in enumerate(positions):
+    enumerated = list(enumerate(left_positions))
+
+    if len(right_positions):
+        offset = len(left_positions) + 1
+        enumerated += list(enumerate(right_positions, start=offset))
+
+    for i, queen_pos in enumerated:
         # New queen is in the same column as this previously placed queen.
         if queen_pos == new_queen_pos:
             return True
@@ -18,7 +24,11 @@ def is_threatened(positions, new_queen_pos):
         # any of the previously placed queens is equal to the absolute
         # difference between their respective row positions, it means they
         # are on the same diagonal and can threaten each other."
-        if abs(queen_pos - new_queen_pos) == abs(i - len(positions)):
+        if abs(queen_pos - new_queen_pos) == abs(i - len(left_positions)):
             return True
 
     return False
+
+
+def free_queens_kmf(positions):
+    return 8 - sum(is_threatened(positions[:i], positions[i], positions[i+1:]) for i in range(8))
