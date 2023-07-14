aac84debc53328086d5d6b81e037b022e98f2705
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-14 14:49:02 -0700

    partial implementation of min-confl algo component funcs

--- codingdojo-org_kata-14_eight-queens/eight_queens_min_confl.py
+++ codingdojo-org_kata-14_eight-queens/eight_queens_min_confl.py
@@ -7,8 +7,8 @@ from math import inf as infinity
 from eight_queens_util import free_queens_kmf as free_queens, is_threatened_by_index, gen_moves_plot
 
 
-__all__ = ("minimum_conflicts", "initialize_positions", "is_solution", "randomly_select_conflicting_queen",
-           "move_queen_to_minimum_conflict_position", "count_conflicts", "move_queen")
+__all__ = ("minimum_conflicts", "rand_positions", "is_solution", "randomly_select_conflicting_queen",
+           "move_queen_to_min_confl_pos", "count_conflicts", "move_queen")
 
 
 # Pseudocode for minimum conflicts random walk algorithm to solve eight queens
@@ -18,40 +18,43 @@ def minimum_conflicts(n, max_steps):
     pass
 
 # function minimum_conflicts(n, max_steps):
-#     board = initialize_positions(n)
+#     board = rand_positions(n)
 #     for i in range(max_steps):
 #         if is_solution(board):
 #             return board
 #         queen = randomly_select_conflicting_queen(board)
-#         move_queen_to_minimum_conflict_position(queen, board)
+#         move_queen_to_min_confl_pos(queen, board)
 #     return None  # No solution found within max_steps
 
-def initialize_positions():
+def rand_positions():
     positions = list(range(8))
     random.shuffle(positions)
     return positions
 
+
 def is_solution(positions):
     return free_queens(positions) == 8
+
 
 def randomly_select_conflicting_queen(positions):
     return random.choice(list(filter(lambda queen_index: is_threatened_by_index(positions, queen_index), range(8))))
 
-def move_queen_to_minimum_conflict_position(queen, board):
-    pass
 
-# function move_queen_to_minimum_conflict_position(queen, board):
-#     min_conflicts = infinity
-#     min_conflict_positions = empty list
-#     for each position in queen's column:
-#         conflicts = count_conflicts(queen, position, board)
-#         if conflicts < min_conflicts:
-#             min_conflicts = conflicts
-#             min_conflict_positions = [position]
-#         else if conflicts == min_conflicts:
-#             min_conflict_positions.append(position)
-#     new_position = randomly_select_position_from(min_conflict_positions)
-#     move_queen(queen, new_position, board)
+def move_queen_to_min_confl_pos(positions, queen_index):
+    min_conflicts = count_conflicts(positions, queen_index)
+    min_confl_pos = []
+    for to_index in range(8):
+        if to_index == queen_index:
+            continue
+        new_positions = move_queen(positions, queen_index, to_index)
+        conflicts = count_conflicts(new_positions, to_index)
+        if conflicts < min_conflicts:
+            min_conflicts = conflicts
+            min_confl_pos = [to_index]
+        else:
+            min_confl_pos.append(to_index)
+    return move_queen(positions, queen_index, random.choice(min_confl_pos))
+
 
 def count_conflicts(positions, queen_index):
     conflicts = 0
@@ -61,11 +64,9 @@ def count_conflicts(positions, queen_index):
         conflicts += moves_plot[row][col]
     return conflicts
 
-def move_queen(queen, position, board):
-    pass
 
-# function move_queen(queen, position, board):
-#     remove queen from its current position on the board
-#     place queen at the new position on the board
-
-
+def move_queen(positions, queen_from_index, queen_to_index):
+    new_positions = positions.copy()
+    new_positions[queen_from_index], new_positions[queen_to_index] = \
+            new_positions[queen_to_index], new_positions[queen_from_index]
+    return new_positions
--- codingdojo-org_kata-14_eight-queens/test_eight_queens.py
+++ codingdojo-org_kata-14_eight-queens/test_eight_queens.py
@@ -4,8 +4,8 @@ from eight_queens import eight_queens_dfs, eight_queens_bfs, eight_queens_sim_an
 
 from eight_queens_util import gen_moves_plot
 
-from eight_queens_min_confl import (minimum_conflicts, initialize_positions, is_solution,
-                                    randomly_select_conflicting_queen, move_queen_to_minimum_conflict_position,
+from eight_queens_min_confl import (minimum_conflicts, rand_positions, is_solution,
+                                    randomly_select_conflicting_queen, move_queen_to_min_confl_pos,
                                     count_conflicts, move_queen)
 
 
@@ -29,6 +29,75 @@ def test_eight_queens_bfs():
 
 
 def test_eight_queens_sim_anneal():
-    for i in range(100):
+    for i in range(10):
         soln = eight_queens_sim_anneal()
         _test_solution(soln)
+
+
+def test_is_solution():
+    known_good_solutions = [[0, 4, 7, 5, 2, 6, 1, 3], [0, 5, 7, 2, 6, 3, 1, 4], [0, 6, 3, 5, 7, 1, 4, 2],
+                            [0, 6, 4, 7, 1, 3, 5, 2], [1, 3, 5, 7, 2, 0, 6, 4], [1, 4, 6, 0, 2, 7, 5, 3],
+                            [1, 4, 6, 3, 0, 7, 5, 2], [1, 5, 0, 6, 3, 7, 2, 4], [1, 5, 7, 2, 0, 3, 6, 4],
+                            [1, 6, 2, 5, 7, 4, 0, 3], [1, 6, 4, 7, 0, 3, 5, 2], [1, 7, 5, 0, 2, 4, 6, 3],
+                            [2, 0, 6, 4, 7, 1, 3, 5], [2, 4, 1, 7, 0, 6, 3, 5], [2, 4, 1, 7, 5, 3, 6, 0],
+                            [2, 4, 6, 0, 3, 1, 7, 5], [2, 4, 7, 3, 0, 6, 1, 5], [2, 5, 1, 4, 7, 0, 6, 3],
+                            [2, 5, 1, 6, 0, 3, 7, 4], [2, 5, 1, 6, 4, 0, 7, 3], [2, 5, 3, 0, 7, 4, 6, 1],
+                            [2, 5, 3, 1, 7, 4, 6, 0], [2, 5, 7, 0, 3, 6, 4, 1], [2, 5, 7, 0, 4, 6, 1, 3],
+                            [2, 5, 7, 1, 3, 0, 6, 4], [2, 6, 1, 7, 4, 0, 3, 5], [2, 6, 1, 7, 5, 3, 0, 4],
+                            [2, 7, 3, 6, 0, 5, 1, 4], [3, 0, 4, 7, 1, 6, 2, 5], [3, 0, 4, 7, 5, 2, 6, 1],
+                            [3, 1, 4, 7, 5, 0, 2, 6], [3, 1, 6, 2, 5, 7, 0, 4], [3, 1, 6, 2, 5, 7, 4, 0],
+                            [3, 1, 6, 4, 0, 7, 5, 2], [3, 1, 7, 4, 6, 0, 2, 5], [3, 1, 7, 5, 0, 2, 4, 6],
+                            [3, 5, 0, 4, 1, 7, 2, 6], [3, 5, 7, 1, 6, 0, 2, 4], [3, 5, 7, 2, 0, 6, 4, 1],
+                            [3, 6, 0, 7, 4, 1, 5, 2], [3, 6, 2, 7, 1, 4, 0, 5], [3, 6, 4, 1, 5, 0, 2, 7],
+                            [3, 6, 4, 2, 0, 5, 7, 1], [3, 7, 0, 2, 5, 1, 6, 4], [3, 7, 0, 4, 6, 1, 5, 2],
+                            [3, 7, 4, 2, 0, 6, 1, 5], [4, 0, 3, 5, 7, 1, 6, 2], [4, 0, 7, 3, 1, 6, 2, 5],
+                            [4, 0, 7, 5, 2, 6, 1, 3], [4, 1, 3, 5, 7, 2, 0, 6], [4, 1, 3, 6, 2, 7, 5, 0],
+                            [4, 1, 5, 0, 6, 3, 7, 2], [4, 1, 7, 0, 3, 6, 2, 5], [4, 2, 0, 5, 7, 1, 3, 6],
+                            [4, 2, 0, 6, 1, 7, 5, 3], [4, 2, 7, 3, 6, 0, 5, 1], [4, 6, 0, 2, 7, 5, 3, 1],
+                            [4, 6, 0, 3, 1, 7, 5, 2], [4, 6, 1, 3, 7, 0, 2, 5], [4, 6, 1, 5, 2, 0, 3, 7],
+                            [4, 6, 1, 5, 2, 0, 7, 3], [4, 6, 3, 0, 2, 7, 5, 1], [4, 7, 3, 0, 2, 5, 1, 6],
+                            [4, 7, 3, 0, 6, 1, 5, 2], [5, 0, 4, 1, 7, 2, 6, 3], [5, 1, 6, 0, 2, 4, 7, 3],
+                            [5, 1, 6, 0, 3, 7, 4, 2], [5, 2, 0, 6, 4, 7, 1, 3], [5, 2, 0, 7, 3, 1, 6, 4],
+                            [5, 2, 0, 7, 4, 1, 3, 6], [5, 2, 4, 6, 0, 3, 1, 7], [5, 2, 4, 7, 0, 3, 1, 6],
+                            [5, 2, 6, 1, 3, 7, 0, 4], [5, 2, 6, 1, 7, 4, 0, 3], [5, 2, 6, 3, 0, 7, 1, 4],
+                            [5, 3, 0, 4, 7, 1, 6, 2], [5, 3, 1, 7, 4, 6, 0, 2], [5, 3, 6, 0, 2, 4, 1, 7],
+                            [5, 3, 6, 0, 7, 1, 4, 2], [5, 7, 1, 3, 0, 6, 4, 2], [6, 0, 2, 7, 5, 3, 1, 4],
+                            [6, 1, 3, 0, 7, 4, 2, 5], [6, 1, 5, 2, 0, 3, 7, 4], [6, 2, 0, 5, 7, 4, 1, 3],
+                            [6, 2, 7, 1, 4, 0, 5, 3], [6, 3, 1, 4, 7, 0, 2, 5], [6, 3, 1, 7, 5, 0, 2, 4],
+                            [6, 4, 2, 0, 5, 7, 1, 3], [7, 1, 3, 0, 6, 4, 2, 5], [7, 1, 4, 2, 0, 6, 3, 5],
+                            [7, 2, 0, 5, 1, 4, 6, 3], [7, 3, 0, 2, 5, 1, 6, 4]]
+    for i in range(1000):
+        positions = rand_positions()
+        assert (positions in known_good_solutions and is_solution(positions)) \
+                or (positions not in known_good_solutions and not is_solution(positions))
+
+
+def test_count_conflicts():
+    assert count_conflicts([6, 4, 2, 0, 5, 7, 1, 3], 7) == 0
+    assert count_conflicts([4, 5, 6, 7, 0, 2, 3, 1], 5) == 1
+    assert count_conflicts([0, 6, 5, 1, 3, 2, 7, 4], 2) == 3
+    assert count_conflicts([4, 1, 7, 6, 5, 2, 3, 0], 3) == 3
+    assert count_conflicts([0, 4, 3, 2, 5, 1, 7, 6], 2) == 4
+
+
+def test_move_queen():
+    positions_1 =       [6, 4, 2, 0, 5, 7, 1, 3]
+    moved_positions_1 = [6, 4, 7, 0, 5, 2, 1, 3]
+    assert move_queen(positions_1, 2, 5) == moved_positions_1
+
+    positions_2 = [4, 5, 6, 7, 0, 2, 3, 1]
+    moved_positions_2 = [1, 5, 6, 7, 0, 2, 3, 4]
+    assert move_queen(positions_2, 0, 7) == moved_positions_2
+
+    positions_3 = [0, 6, 5, 1, 3, 2, 7, 4]
+    moved_positions_3 = [0, 6, 5, 3, 1, 2, 7, 4]
+    assert move_queen(positions_3, 3, 4) == moved_positions_3
+
+    positions_4 = [4, 1, 7, 6, 5, 3, 2, 0]
+    moved_positions_4 = [4, 1, 7, 6, 5, 2, 3, 0]
+    assert move_queen(positions_4, 5, 6) == moved_positions_4
+
+    positions_5 = [6, 4, 3, 2, 5, 1, 7, 0]
+    moved_positions_5 = [0, 4, 3, 2, 5, 1, 7, 6]
+    assert move_queen(positions_5, 0, 7) == moved_positions_5
+
