75799b8c977a12aef48c271c66a174ec2105d87c
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-18 14:27:29 -0700

    added support for 0 => '*' requirement, tests passes

--- codingdojo-org_kata-18_foobarqix/foobarqix.py
+++ codingdojo-org_kata-18_foobarqix/foobarqix.py
@@ -10,11 +10,19 @@ def compute(intstr):
         towrite.append("Bar")
     if intval % 7 == 0:
         towrite.append("Qix")
-    for char in intstr:
-        if char == "3":
-            towrite.append("Foo")
-        elif char == "5":
-            towrite.append("Bar")
-        elif char == "7":
-            towrite.append("Qix")
+    if set(intstr) & set("357"):
+        for char in intstr:
+            if char == "3":
+                towrite.append("Foo")
+            elif char == "5":
+                towrite.append("Bar")
+            elif char == "7":
+                towrite.append("Qix")
+            elif char == "0":
+                towrite.append("*")
+    elif len(towrite):
+        if "0" in intstr:
+            towrite.append("*" * intstr.count("0"))
+    else:
+        intstr = intstr.replace("0", "*")
     return str.join("", towrite) if towrite else intstr
--- codingdojo-org_kata-18_foobarqix/test_foobarqix.py
+++ codingdojo-org_kata-18_foobarqix/test_foobarqix.py
@@ -14,10 +14,17 @@ def test_compute_1():
     assert compute("7") == "QixQix"
     assert compute("8") == "8"
     assert compute("9") == "Foo"
-    assert compute("10") == "Bar"
+    assert compute("10") == "Bar*"
     assert compute("13") == "Foo"
     assert compute("15") == "FooBarBar"
     assert compute("21") == "FooQix"
     assert compute("33") == "FooFooFoo"
     assert compute("51") == "FooBar"
     assert compute("53") == "BarFoo"
+
+
+def test_compute_2():
+    assert compute("101") == "1*1"
+    assert compute("303") == "FooFoo*Foo"
+    assert compute("105") == "FooBarQix*Bar"
+    assert compute("10101") == "FooQix**"
