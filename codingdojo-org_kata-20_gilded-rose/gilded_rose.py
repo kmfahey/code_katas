7f335f07e62e5d7347e8f3d2a42ae61349a9f493
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-28 15:25:20 -0700

    brought update_quality into line with specs in kata

--- codingdojo-org_kata-20_gilded-rose/gilded_rose.py
+++ codingdojo-org_kata-20_gilded-rose/gilded_rose.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import operator
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -9,27 +11,48 @@ class GildedRose(object):
         for item in self.items:
             match item.name:
                 case "Aged Brie":
-                    item.quality += 1
+                    # “Aged Brie” actually increases in Quality the older it gets
+                    # The Quality of an item is never more than 50
+                    if item.quality < 50:
+                        item.quality += 1
                     item.sell_in -= 1
-                    if item.sell_in < 0:
+                    if item.sell_in < 0 and item.quality < 50:
                         item.quality += 1
                 case "Backstage passes to a TAFKAL80ETC concert":
+                    # “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches
+                    # The Quality of an item is never more than 50
                     if item.quality < 50:
+                        # Quality increases by 3 when there are 5 days or less
                         if item.sell_in < 6:
                             item.quality += 3
+                        # Quality increases by 2 when there are 10 days or less
                         elif item.sell_in < 11:
                             item.quality += 2
+                        # but Quality drops to 0 after the concert
                         else:
                             item.quality += 1
                     item.sell_in -= 1
                     if item.sell_in < 0:
                         item.quality = 0
                 case "Sulfuras, Hand of Ragnaros":
+                    # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
                     pass
-                case _:
-                    item.quality -= 1
+                case name if name.startswith("Conjured"):
                     item.sell_in -= 1
-                    if item.sell_in < 0:
+                    # if the sell-by date has passed, quality degrades twice as fast.
+                    # The Quality of an item is never negative
+                    # “Conjured” items degrade in Quality twice as fast as normal items
+                    if item.sell_in < 0 and item.quality > 1:
+                        item.quality -= 4
+                    elif item.quality > 0:
+                        item.quality -= 2
+                case _:
+                    item.sell_in -= 1
+                    # if the sell-by date has passed, quality degrades twice as fast.
+                    # The Quality of an item is never negative
+                    if item.sell_in < 0 and item.quality > 1:
+                        item.quality -= 2
+                    elif item.quality > 0:
                         item.quality -= 1
 
 
