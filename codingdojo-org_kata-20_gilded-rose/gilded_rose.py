baacbf739bc1cd0205010371a06edc69f40f3f80
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-28 14:46:13 -0700

    clarified conditional tree

--- codingdojo-org_kata-20_gilded-rose/gilded_rose.py
+++ codingdojo-org_kata-20_gilded-rose/gilded_rose.py
@@ -7,33 +7,32 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+            match item.name:
+                case "Aged Brie" if item.quality < 50:
+                    item.quality += 1
+                case "Backstage passes to a TAFKAL80ETC concert" if item.quality < 50:
+                    if item.sell_in < 6:
+                        item.quality += 3
+                    elif item.sell_in < 11:
+                        item.quality += 2
+                    else:
+                        item.quality += 1
+                case "Sulfuras, Hand of Ragnaros":
+                    pass
+                case _ if item.quality > 0:
+                    item.quality -= 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        item.quality = 0
+                    elif item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+                        item.quality -= 1
+                elif item.quality < 50:
+                    item.quality += 1
 
 
 class Item:
