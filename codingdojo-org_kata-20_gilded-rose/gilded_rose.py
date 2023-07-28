ed3539e96b14ba62d0e8c8fe2b417bcba477d0f7
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-07-28 15:05:38 -0700

    replaced update quality conditional tree with match/case statement

--- codingdojo-org_kata-20_gilded-rose/gilded_rose.py
+++ codingdojo-org_kata-20_gilded-rose/gilded_rose.py
@@ -8,31 +8,29 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             match item.name:
-                case "Aged Brie" if item.quality < 50:
+                case "Aged Brie":
                     item.quality += 1
-                case "Backstage passes to a TAFKAL80ETC concert" if item.quality < 50:
-                    if item.sell_in < 6:
-                        item.quality += 3
-                    elif item.sell_in < 11:
-                        item.quality += 2
-                    else:
+                    item.sell_in -= 1
+                    if item.sell_in < 0:
                         item.quality += 1
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality < 50:
+                        if item.sell_in < 6:
+                            item.quality += 3
+                        elif item.sell_in < 11:
+                            item.quality += 2
+                        else:
+                            item.quality += 1
+                    item.sell_in -= 1
+                    if item.sell_in < 0:
+                        item.quality = 0
                 case "Sulfuras, Hand of Ragnaros":
                     pass
-                case _ if item.quality > 0:
+                case _:
                     item.quality -= 1
-
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = 0
-                    elif item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+                    item.sell_in -= 1
+                    if item.sell_in < 0:
                         item.quality -= 1
-                elif item.quality < 50:
-                    item.quality += 1
 
 
 class Item:
